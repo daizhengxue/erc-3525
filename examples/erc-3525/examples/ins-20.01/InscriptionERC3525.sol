8d73a629b2e41a383c29962f36ebd684e2f4f391
daizhengxue <daizhengxue828@gmail.com>
2023-12-27 00:47:56 +0800

    sync

--- examples/erc-3525/examples/ins-20.01/InscriptionERC3525.sol
+++ examples/erc-3525/examples/ins-20.01/InscriptionERC3525.sol
@@ -4,11 +4,12 @@ import "@openzeppelin/contracts/utils/Strings.sol";
 import "@solvprotocol/erc-3525/ERC3525.sol";
 import "@solvprotocol/erc-3525/IERC3525.sol";
 import "./Base64.sol";
-contract InscriptionERC3525 is ERC3525 {
+contract InscriptionTrigram is ERC3525 {
     uint256 public mintLimit;
     uint256 public maxSupply;
     uint128 internal _totalSupply;
-    //坤为地：全阴，二进制表示为 '000'。
+    //乾为天：阳阳阳，二进制表示为 '111'
+    //坤为地：阴阴阴，二进制表示为 '000'。
     //震为雷：阳阴阴，二进制表示为 '100'。
     //巽为风：阴阳阴，二进制表示为 '010'。
     //坎为水：阴阳阳，二进制表示为 '011'。
@@ -16,46 +17,38 @@ contract InscriptionERC3525 is ERC3525 {
     //艮为山：阴阴阳，二进制表示为 '001'。
     //兑为泽：阳阳阴，二进制表示为 '110'。
     uint8[8] private hexagrams = [7, 0, 4, 2, 3, 5, 1, 6];
-    uint8 private nextSlot = 1;  // start at 1
+    uint8 private nextSlot = 1;  // 从 1 开始
     mapping(address => uint256) private addressToSlot;
 
     using Strings for uint256;
 
-    // Struct to represent an Inscription, which is a set of slots
-    // struct Inscription {
-    //   string op;     // Operation, e.g., "mint"
-    // uint256 amt;   }
-    // Mapping from tokenId to its Inscription data
-    //mapping(uint256 => Inscription) private _inscriptions;
-
     constructor(    
         uint64 maxSupply_,
         uint64  mintLimit_
-    ) ERC3525("Ins-20", "FINA", 18) {
+    ) ERC3525("Trigram", "TGT", 18) {
 
         maxSupply = maxSupply_;
         mintLimit = mintLimit_;
         nextSlot = 1;
+
     }
-       function InscriptionMinted(uint256 amt) public {
+       function Inscribe(uint256 amt) public {
     require(tx.origin == msg.sender, "Contracts are not allowed");
     require(amt <= mintLimit, "Exceeded mint limit");
     require(_totalSupply + amt <= maxSupply, "Exceeded max supply");
 
-    uint256 slot;
+    uint8 slot;
     if (addressToSlot[msg.sender] == 0) {
-        // 使用区块时间戳和发送者地址生成伪随机数
         uint256 random = uint256(keccak256(abi.encodePacked(block.timestamp, msg.sender))) % 8;
-        slot = hexagrams[random];  // 从易经卦象中选择一个 slot
+        slot = hexagrams[random];  // 从易经卦象中选择一个 slot 
         addressToSlot[msg.sender] = slot;
     } else {
-        slot = addressToSlot[msg.sender];  // reuse slot
+        slot = uint8(addressToSlot[msg.sender]); //当铭文处于同一个卦象时你可以和同卦象的铭文进行价值的堆叠
     }
 
-    // Mint 
+    // 篆
     _mint(msg.sender, slot, amt);
 
-    // Update total supply
     _totalSupply += uint128(amt);
 }
 
@@ -69,7 +62,6 @@ contract InscriptionERC3525 is ERC3525 {
     require(_exists(tokenId), "Query for nonexistent token");
 
     // Fetch the inscription data for the given token
-    //Inscription memory inscription = _inscriptions[tokenId];
     uint256 currentBalance = balanceOf(tokenId);
     string memory op = "mint";
     string memory tick = symbol();
@@ -98,7 +90,7 @@ contract InscriptionERC3525 is ERC3525 {
             string(
                 abi.encodePacked(
                     '{"name":"', tick, ' #', Strings.toString(tokenId), '",',
-                    '"description":"An ERC3525 token with inscription.",',
+                    '"description":"One creats two, tow create three",',
                     '"image":"', imageURI, '"}'
                 )
             )
@@ -108,7 +100,5 @@ contract InscriptionERC3525 is ERC3525 {
     // Return the final data URI
     return string(abi.encodePacked("data:application/json;base64,", json));
     }
-
-    // Add other functions as needed...
 
 }
